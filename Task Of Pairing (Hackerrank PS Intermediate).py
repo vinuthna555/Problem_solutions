630a5ad0f36e1348ea1f92188333773ad6f5c673
vinuthna555 <59686452+vinuthna555@users.noreply.github.com>
2021-06-15 16:01:59 +0530

    Update and rename Task Of Pairing (Hackerrank PS Intermediate) to Task Of Pairing (Hackerrank PS Intermediate).py

---
+++ Task Of Pairing (Hackerrank PS Intermediate).py
@@ -0,0 +1,43 @@
+!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'taskOfPairing' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY freq as parameter.
+#
+
+def taskOfPairing(freq):
+    ans=0
+    n=len(freq)
+    for i in range(0,len(freq)-1):
+        ans+=freq[i]//2
+        if freq[i+1]!=0:
+            freq[i+1]+=freq[i]%2
+    return ans+freq[n-1]//2
+    
+    # Write your code here
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    freq_count = int(input().strip())
+
+    freq = []
+
+    for _ in range(freq_count):
+        freq_item = int(input().strip())
+        freq.append(freq_item)
+
+    result = taskOfPairing(freq)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
